140296042075e76e78234038e6b8ace069e927f7
hamidashi-chin <take106141@gmail.com>
2024-03-25 01:37:37 +0900

    はじめてつくるバックエンドサーバー PUTリクエストの前まで

--- express_intro/html.js
+++ express_intro/html.js
@@ -1,13 +1,21 @@
 // index.js
 
+const fs = require('fs');
 const express =  require("express");
 const app = express();
+app.use(express.urlencoded({ extends: true }));
 
 coronaData = require("./coronaData.json");
 
 app.get("/", function (req, res) {
   console.log("__dirnameの中身", __dirname);
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/autumn", function (req, res) {
+  fs.writeFile(__dirname + "/data.txt", req.body.activity, function () {
+    res.send("投稿完了");
+  });
 });
 
 app.listen(5000, function() {
